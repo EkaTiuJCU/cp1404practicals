9198dc51d903e969172759482c2a5ba70392a647
Asus <ekadiantara.tiu@my.jcu.edu.au>
2025-06-01 21:01:06 +0800

    Delete some space

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,7 @@
 MINIMUM_LENGTH = 4
-
-
 def main():
     password = get_password(MINIMUM_LENGTH)
     print_asterisks(password)
-
 
 def get_password(minimum_length):
     password = input(f"Enter password of at least {minimum_length} characters: ")
