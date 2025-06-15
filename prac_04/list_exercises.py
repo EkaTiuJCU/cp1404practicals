54f1b0a4ad6f609612fb72f2f41938d509d55c4d
Asus <ekadiantara.tiu@my.jcu.edu.au>
2025-06-15 21:00:59 +0800

    Add username authorization check

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -11,5 +11,14 @@ print(f"The largest number is {max(numbers)}")
 average = sum(numbers) / len(numbers)
 print(f"The average of the numbers is {average}")
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
+user_input = input("Enter your username: ")
+
+if user_input in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
 
