d46c6dc7e93aed511c2b5fb9482739591d5e691d
Asus <ekadiantara.tiu@my.jcu.edu.au>
2025-07-06 21:27:22 +0800

    Complete ProgrammingLanguage class with test function
    - Added __init__, __str__, and is_dynamic methods
    - Included run_tests() to demonstrate functionality
    - Checked for dynamic typing and printed results
    - Added docstrings and structure for readability

---
+++ prac_06/programming_language.py
@@ -0,0 +1,19 @@
+"""
+Estimate: 10 minutes
+Actual: 15 minutes
+"""
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.typing.lower() == "dynamic"
