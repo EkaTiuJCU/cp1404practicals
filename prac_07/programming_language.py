2253a259e823ba119b6686f78699c7d79626d35f
Asus <ekadiantara.tiu@my.jcu.edu.au>
2025-07-07 18:37:25 +0800

    add () in line 20 and 21

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -17,8 +17,8 @@ class ProgrammingLanguage:
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection},"
-               f"Pointer Arithemitc={self.pointer_aritmethic}, First appeared in {self.year}")
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, "
+                f"Pointer Arithmetic={self.pointer_arithmetic}, First appeared in {self.year}")
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
@@ -27,9 +27,9 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, False)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
     c_plus_plus = ProgrammingLanguage("C++", "Static", True, 1985, True)
 
     languages = [ruby, python, visual_basic, c_plus_plus]
